b86a022d1824f9e5d5777de55f0d8996eb737bd0
akgrenSoar <44816346+akgrenSoar@users.noreply.github.com>
2020-05-17 21:16:34 +0800

    Trivial

--- app.js
+++ app.js
@@ -13,18 +13,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Parse requests
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Response 200
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404)); // Fall thru
 });
 
 // error handler
