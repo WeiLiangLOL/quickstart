35cc9c5508fd9f591e6436892df9038a98369a5f
WeiLiang <kuahweiliang@hotmail.com>
2020-05-22 14:53:17 +0800

    Heroku environment patch

--- database/index.js
+++ database/index.js
@@ -10,15 +10,14 @@ let sequelize;
  * @param {Express.Application} app the express application 
  */
 function init(app) {
-    service.start(() => {
-        debug('Database connection established');
-
-        if (process.env.NODE_ENV === 'production') {
-            // TODO: Haven't set this up
-        } else {
+    if (process.env.NODE_ENV === 'production') {
+        // TODO: Not implemented
+    } else {
+        service.start(() => {
+            debug('Database connection established');
             sequelize = new Sequelize('postgres://postgre:22Fast+++@example.com:5432/dbname');
-        }
-    });
+        });
+    }
 }
 
 module.exports = {
